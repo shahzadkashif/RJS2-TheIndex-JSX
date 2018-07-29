c72495d4a364c4fd124cdf1cb175fae9b4f122c0
Octowl <alsaff1987@gmail.com>
2018-07-29 14:05:18 +0300

    Fix sidebar

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ class App extends Component {
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <sidebar>
+            <div id="sidebar">
               <img src="theindex.svg" className="logo" />
               <section>
                 <h4 className="menu-item active">
                   <a href="#">AUTHORS</a>
                 </h4>
               </section>
-            </sidebar>
+            </div>
           </div>
           <div className="content col-10">
             <div className="authors">
@@ -31,7 +31,7 @@ class App extends Component {
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>
-                          {authors[0].firstName + " " + authors[0].lastName}
+                          {authors[0].first_name + " " + authors[0].last_name}
                         </span>
                       </h5>
                       <small className="card-text">
@@ -51,7 +51,7 @@ class App extends Component {
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>
-                          {authors[1].firstName + " " + authors[1].lastName}
+                          {authors[1].first_name + " " + authors[1].last_name}
                         </span>
                       </h5>
                       <small className="card-text">
@@ -71,7 +71,7 @@ class App extends Component {
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>
-                          {authors[2].firstName + " " + authors[2].lastName}
+                          {authors[2].first_name + " " + authors[2].last_name}
                         </span>
                       </h5>
                       <small className="card-text">
