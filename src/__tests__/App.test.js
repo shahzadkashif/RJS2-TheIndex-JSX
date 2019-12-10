3e08c7fcf2e254a1179b45fe03f268b690c12086
Octowl <alsaff1987@gmail.com>
2019-12-10 18:10:43 +0300

    beforeEach

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { mount } from "enzyme";
-import toJSON from "enzyme-to-json";
 
 import authors from "../data";
 
 import App from "../App";
 
 describe("<App />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+
   it("renders correctly", () => {
-    const wrapper = mount(<App />);
     expect(wrapper.find("#app").exists()).toBe(true);
   });
 
   it("is not just an empty div", () => {
-    const wrapper = mount(<App />);
     expect(wrapper.find("#app").children().length).toBe(1);
   });
 
   it("renders the right number of authors", () => {
-    const wrapper = mount(<App />);
     expect(wrapper.find(".card").length).toBe(4);
   });
 
   it("renders different authors", () => {
-    const wrapper = mount(<App />);
     const cards = wrapper
       .find(".card")
       .map(card => card.find(".card-title").text());
@@ -31,7 +32,6 @@ describe("<App />", () => {
   });
 
   it("has the correct content in each author card", () => {
-    const wrapper = mount(<App />);
     wrapper.find(".card").forEach((card, idx) => {
       const image = card.find("img");
       const authorName = card.find(".card-title");
